6ddeca0d44e6f3f1c252c550e5e0e1d9c1458494
Willinton Mora <wifermen@hotmail.com>
2024-05-31 06:33:05 -0500

    chore: update menu.js and global.css

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,8 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-    document.querySelector(".nav-links").classList.toggle("expanded");
+
+const hamburguer = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburguer.addEventListener("click", () => {
+    navLinks.classList.toggle("expanded");
+   hamburguer.classList.toggle("active");
 });
